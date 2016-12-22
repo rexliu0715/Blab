f242c62bb261c8d8daf788b88af8804b0bf25dda
Rex Liu <rexliu0715@gmail.com>
2016-12-22 17:41:31 +0800

    fix admin post input

--- wp-admin/includes/admin-filters.php
+++ wp-admin/includes/admin-filters.php
@@ -120,7 +120,7 @@ add_action( 'upgrader_process_complete', 'wp_version_check', 10, 0 );
 add_action( 'upgrader_process_complete', 'wp_update_plugins', 10, 0 );
 add_action( 'upgrader_process_complete', 'wp_update_themes', 10, 0 );
 
-if ($current_user->roles != 'administrator' || $current_user->roles != 'superadmin') {
+if ($current_user->roles != 'administrator') {
 	// Remove meta box
 	add_action( 'admin_menu', 'remove_my_post_metaboxes' );	
 	add_action( 'do_meta_boxes', 'remove_plugin_metaboxes' );
